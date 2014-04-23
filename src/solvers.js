4c9dece70a01c1f66d9e186fc8519ddad5a1e475
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-23 16:54:54 -0700

    started working on N-Rooks solver

--- src/solvers.js
+++ src/solvers.js
@@ -14,16 +14,53 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var results = {};
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var countPieces = function(board) {
+    var n = board.rows().length;
+    var counter = 0;
+    var matrix = board.rows();
+    for (var i = 0; i < n; i++) {
+      for (var j = 0 ; j < n; j++){
+        counter += matrix[i][j];
+      }
+    }
+    return counter;
+  };
+
+  var  addPiece = function (board) {
+    var pieceCount = countPieces(board);
+    console.log('piececount: ' + pieceCount);
+    if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()){
+      return;
+    } else if (pieceCount === n) {
+
+      results[JSON.stringify(board.rows())] = board.rows();
+      return;
+    } else if (pieceCount > n) {
+      return;
+    }
+    for (var i = 0; i < n; i++){
+      for (var j = 0; j < n; j++){
+        if (board.get(i)[j]  === 0) {
+          var copy = board.rows().slice(0);
+          copy[i][j] = 1;
+          addPiece(new Board(copy));
+
+        }
+      }
+    }
+  };
+
+  addPiece(new Board({'n': n}));
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return results;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
+window.countNRooksSolutions = function (n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
