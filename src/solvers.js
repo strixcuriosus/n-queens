2f639ee4204b33044426bd2edaa8b267b76a25b3
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-24 21:02:33 -0700

    Refactored to improve code readability.

--- src/solvers.js
+++ src/solvers.js
@@ -6,11 +6,6 @@
 
 */
 
-// hint: you'll need to do a full-search of all possible arrangements of pieces!
-// (There are also optimizations that will allow you to skip a lot of the dead search space)
-// take a look at solversSpec.js to see what the tests are expecting
-
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
@@ -33,15 +28,15 @@ window.countNRooksSolutions = function (n) {
     var set =[];
 
     var permute = function (arr, data) {
-      var cur, memo = data || [];
+      var current, memo = data || [];
 
       for (var i = 0; i < arr.length; i++) {
-        cur = arr.splice(i, 1)[0];
+        current = arr.splice(i, 1)[0];
         if (arr.length === 0) {
-          set.push(memo.concat([cur]));
+          set.push(memo.concat([current]));
         }
-        permute(arr.slice(), memo.concat([cur]));
-        arr.splice(i, 0, cur);
+        permute(arr.slice(), memo.concat([current]));
+        arr.splice(i, 0, current);
       }
       return set;
     };
@@ -50,57 +45,7 @@ window.countNRooksSolutions = function (n) {
   return permutator(matrix).length;
 };
 
-  // var solutionCount = undefined; //fixme
-
-  // var results = {};
-
-  // var countPieces = function(board) {
-  //   var n = board.rows().length;
-  //   var counter = 0;
-  //   var matrix = board.rows();
-  //   for (var i = 0; i < n; i++) {
-  //     for (var j = 0 ; j < n; j++){
-  //       counter += matrix[i][j];
-  //     }
-  //   }
-  //   return counter;
-  // };
-
-  // var  addPiece = function (board) {
-  //   var pieceCount = countPieces(board);
-  //   console.log('piececount: ' + pieceCount);
-  //   if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()){
-  //     return;
-  //   }
-  //   if (pieceCount >= n) {
-  //     results[JSON.stringify(board.rows())] = board.rows();
-  //     return;
-  //   // } else if (pieceCount > n) {
-  //   //   return;
-  //   }
-  //   for (var i = 0; i < n; i++){
-  //     for (var j = 0; j < n; j++){
-  //       if (board.get(i)[j]  === 0) {
-  //         var copy = board.rows().slice(0);
-  //         copy[i][j] = 1;
-  //         addPiece(new Board(copy));
-
-  //       }
-  //     }
-  //   }
-  // };
-
-  // addPiece(new Board({'n': n}));
-  // // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  // return results;
-  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  // return solutionCount;
-
-
-//////////////////////////////////
-/// buggy first attempt at n-rooks problem:
-
-window.AllRooksSolutions = function (n) {
+window.getAllRooksSolutions = function (n) {
   var matrix =  findNRooksSolution(n);
 
   var permutator = function (input) {
@@ -124,11 +69,6 @@ window.AllRooksSolutions = function (n) {
   return permutator(matrix);
 };
 
-
-
-
-
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   if (n === 0) {
     return [];
@@ -138,9 +78,8 @@ window.findNQueensSolution = function(n) {
     var ans = new Board({'n':n});
     return ans.rows();
   } else {
-    var possibleSolutionSet =  AllRooksSolutions(n);
+    var possibleSolutionSet =  getAllRooksSolutions(n);
     return _.reduce(possibleSolutionSet, function(memo, matrix){
-      debugger;
       if( memo === 0 ){
         var currentBoard = new Board(matrix);
         if (! currentBoard.hasAnyQueensConflicts() ){
@@ -152,84 +91,33 @@ window.findNQueensSolution = function(n) {
         return memo;
       }
     }, 0);
-  };
-  //   var board = new Board(findNRooksSolution(n));
-  //   while (board.hasAnyRowConflicts() || board.hasAnyColConflicts() || board.hasAnyMinorDiagonalConflicts() || board.hasAnyMajorDiagonalConflicts()){
-  //     board = new Board(_.shuffle(board.rows()));
-  //   }
-  //     // console.log(board.rows());
-  //   return board.rows();
-  // }
+  }
 };
-  // var hasNoDiagConflict = function (matrix) {
-  //   var flag = true;
-  //   var coords = [];
-  //   for (var i = 0; i < n; i++){
-  //     for (var j = 0; j < n; j++) {
-  //       if (matrix[i][j]) coords.push([i,j]);
-  //     }
-  //   }
-  //   coords.forEach(function (p1, i) {
-  //     coords.forEach(function (p2, j) {
-  //       if (i !== j) {
-  //         //var slope = p1[1] - p2[1] / p1[0] - p2[0];
-  //         if (Math.abs(p1[1] - p2[1]) === Math.abs(p1[0] - p2[0])) {
-  //           flag = false;
-  //         }
-  //       }
 
-  //     });
-  //   });
-  //   return flag;
-  // };
-
-  // if (n === 0) {
-  //   return [];
-  // } else if(n === 1) {
-  //   return [1];
-  // } else {
-  //   var rooksolns = AllRooksSolutions(n);
-  //   for (var j = 0; j < rooksolns.length; j++ ) {
-  //     if (hasNoDiagConflict(rooksolns[j])){
-  //       return rooksolns[j];
-  //     }
-  //   }
-  // }
-
-
-
-  // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  // return solution;
-  //
-
-
-// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   if (n === 1 || n === 0) {
     return 1;
   }
-  var solutionSet =  AllRooksSolutions(n);
+  var solutionSet =  getAllRooksSolutions(n);
 
   var hasNoDiagConflict = function (matrix) {
-    var flag = true;
     var coords = [];
     for (var i = 0; i < n; i++){
       for (var j = 0; j < n; j++) {
         if (matrix[i][j]) coords.push([i,j]);
       }
     }
-    coords.forEach(function (p1, i) {
-      coords.forEach(function (p2, j) {
-        if (i !== j) {
+    for (var k = 0; k < coords.length; k++){
+      for (var l = 0; l < coords.length; l++){
+        if (k !== l) {
           //var slope = p1[1] - p2[1] / p1[0] - p2[0];
-          if (Math.abs(p1[1] - p2[1]) === Math.abs(p1[0] - p2[0])) {
-            flag = false;
+          if (Math.abs(coords[k][1] - coords[l][1]) === Math.abs(coords[k][0] - coords[l][0])) {
+            return false;
           }
         }
-
-      });
-    });
-    return flag;
+      }
+    }
+    return true;
   };
   return solutionSet.filter(hasNoDiagConflict).length;
 };
